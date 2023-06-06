b70168e5c72d09a490425190f4b207b0221b013e
Adam Merjil <AMerjil@hotmail.com>
2023-06-06 10:24:11 -0700

    modified:   api/index.js 	deleted:    api/models/post.js 	modified:   api/models/user.js 	modified:   client/src/pages/login.js 	modified:   client/src/pages/register.js 	modified:   node_modules/.package-lock.json 	new file:   node_modules/bcryptjs/.npmignore 	new file:   node_modules/bcryptjs/.travis.yml 	new file:   node_modules/bcryptjs/.vscode/settings.json 	new file:   node_modules/bcryptjs/LICENSE 	new file:   node_modules/bcryptjs/README.md 	new file:   node_modules/bcryptjs/bin/bcrypt 	new file:   node_modules/bcryptjs/bower.json     new file:   node_modules/bcryptjs/dist/README.md 	new file:   node_modules/bcryptjs/dist/bcrypt.js 	new file:   node_modules/bcryptjs/dist/bcrypt.min.js 	new file:   node_modules/bcryptjs/dist/bcrypt.min.js.gz 	new file:   node_modules/bcryptjs/dist/bcrypt.min.map 	new file:   node_modules/bcryptjs/externs/bcrypt.js 	new file:   node_modules/bcryptjs/externs/minimal-env.js 	new file:   node_modules/bcryptjs/index.js 	new file:   node_modules/bcryptjs/package.json 	new file:   node_modules/bcryptjs/scripts/build.js 	new file:   node_modules/bcryptjs/src/bcrypt.js 	new file:   node_modules/bcryptjs/src/bcrypt/impl.js 	new file:   node_modules/bcryptjs/src/bcrypt/prng/README.md 	new file:   node_modules/bcryptjs/src/bcrypt/prng/accum.js 	new file:   node_modules/bcryptjs/src/bcrypt/prng/isaac.js 	new file:   node_modules/bcryptjs/src/bcrypt/util.js 	new file:   node_modules/bcryptjs/src/bcrypt/util/base64.js 	new file:   node_modules/bcryptjs/src/bower.json 	new file:   node_modules/bcryptjs/src/wrap.js 	new file:   node_modules/bcryptjs/tests/quickbrown.txt 	new file:   node_modules/bcryptjs/tests/suite.js 	modified:   package-lock.json 	modified:   package.json

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,11 +1,32 @@
 import '../styles/login-reg.scss';
+import {useState} from "react";
 
 export default function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    async function login(ev) {
+        ev.preventDefualt();
+        await fetch('http://localhost:400/login', {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: {'Content-Type':'application/json'},
+        })
+    }
     return(
-        <form className="login">
+        <form className="login" onSubmit={login}>
             <h1>LOGIN</h1>
-            <input type="text" placeholder="username"/>
-            <input type="text" placeholder="password"/>
+            <input 
+                type="text" 
+                placeholder="username" 
+                value={username} 
+                onChange={ev => setUsername(ev.target.value)}
+            />
+            <input 
+                type="text" 
+                placeholder="password" 
+                value={password} 
+                onChange={ev => setPassword(ev.target.value)}
+            />
             <button className="log-btn">login</button>
         </form>
     )
--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,65 +1,35 @@
-import '../styles/login-reg.scss';
-import { useState } from "react";
+import {useState} from "react";
 
-export default function Register() {
+export default function RegisterPage() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-
   async function register(ev) {
     ev.preventDefault();
-    
-    if (password !== confirmPassword) {
-      // Passwords don't match
-      console.error("Passwords don't match");
-      return;
-    }
-
-    try {
-      const response = await fetch('http://localhost:4000/register', {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        // Registration successful, handle the data if needed
-        console.log(data);
-      } else {
-        // Registration failed, handle the error
-        const error = await response.json();
-        console.error(error);
-      }
-    } catch (error) {
-      // Network or server error occurred
-      console.error(error);
+    const response = await fetch('http://localhost:4000/register', {
+      method: 'POST',
+      body: JSON.stringify({username,password}),
+      headers: {'Content-Type':'application/json'},
+    });
+    if (response.status === 200) {
+      alert('registration successful');
+    } else {
+      alert('registration failed');
     }
   }
-
   return (
     <form className="register" onSubmit={register}>
-      <h1>REGISTER</h1>
-      <input
+      <h1>Register</h1>
+      <input 
         type="text"
         placeholder="username"
-        onChange={ev => setUsername(ev.target.value)}
-      />
-      <input
+        value={username}
+        onChange={ev => setUsername(ev.target.value)}/>
+      <input 
         type="password"
         placeholder="password"
-        pattern=".{4,}"
-        required
-        onChange={ev => setPassword(ev.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="re-type password"
-        pattern=".{4,}"
-        required
-        onChange={ev => setConfirmPassword(ev.target.value)}
-      />
-      <button className="reg-btn">Register</button>
+        value={password}
+        onChange={ev => setPassword(ev.target.value)}/>
+      <button>Register</button>
     </form>
   );
 }
